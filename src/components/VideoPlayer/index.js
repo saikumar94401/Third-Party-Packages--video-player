42a9d7c7d2434317deb434412f9c5418f95704b5
saikumar <saikumar.komma@gmail.com>
2023-04-19 16:41:22 +0000

    video player

---
+++ src/components/VideoPlayer/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+import ReactPlayer from 'react-player'
+
+const VideoPlayer = () => (
+  <div className="video-container">
+    <h1 className="heading">Video Player</h1>
+    <div className="responsive-container">
+      <ReactPlayer url="https://www.youtube.com/watch?v=hFaprk_abl0&ab_channel=BeerBiceps" />
+    </div>
+  </div>
+)
+
+export default VideoPlayer
